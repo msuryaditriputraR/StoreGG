9a1657b3d96ce52a602aca5e1cea6a528fb14c25
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2022-11-30 11:33:44 +0800

    scroll up

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,12 @@ navToggle.addEventListener("click", () => {
     console.log(navMenu);
     navMenu.classList.toggle("active-menu");
 });
+
+/*=============== SHOW SCROLL UP ===============*/
+function scrollUp() {
+    const scrollUp = document.getElementById("scroll-up");
+
+    scrollUp.classList[this.scrollY >= 200 ? "add" : "remove"]("show-scroll");
+}
+
+window.addEventListener("scroll", scrollUp);
