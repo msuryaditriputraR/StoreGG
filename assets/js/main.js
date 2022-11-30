b93e4d0c5ebf49aec3c40f5169e801bb48e9ba32
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2022-11-30 13:37:17 +0800

    menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,15 +3,23 @@ const navToggle = document.getElementById("nav-toggle");
 const navMenu = document.getElementById("nav-menu");
 
 navToggle.addEventListener("click", () => {
-    console.log(navMenu);
     navMenu.classList.toggle("active-menu");
+});
+
+/* === CLOSE MENU When menu-item was clicked === */
+const navLinks = document.querySelectorAll(".nav__link");
+
+navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+        navMenu.classList.remove("active-menu");
+    });
 });
 
 /*=============== SHOW / HIDE ELEMENT SCROLL UP ===============*/
 function toggleElement(elementId, elementClass) {
     const element = document.getElementById(elementId);
 
-    element.classList[this.scrollY >= 200 ? "add" : "remove"](elementClass);
+    element.classList[this.scrollY >= 130 ? "add" : "remove"](elementClass);
 }
 
 window.addEventListener("scroll", () => {
@@ -21,3 +29,27 @@ window.addEventListener("scroll", () => {
     /* === Show Scroll === */
     toggleElement("scroll-up", "show-scroll");
 });
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+    const scrollY = window.scrollY;
+
+    sections.forEach((current) => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 58;
+        const sectionId = current.getAttribute("id");
+        const sectionClass = document.querySelector(
+            `.nav__menu a[href*=${sectionId}]`
+        );
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            sectionClass.classList.add("active-link");
+        } else {
+            sectionClass.classList.remove("active-link");
+        }
+    });
+}
+
+window.addEventListener("scroll", scrollActive);
